d8e506ed1e1bc6d19c9c023cf60fc70d918366be
root <par@wennergrund.net>
2026-09-04 21:48:06 +0200

    fortsätt labbande med realm4

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,56 @@ impl SimpleComponent for App {
     view! {
         // Vi använder 'gtk' som kommer från den importerade preluden ovan
         gtk::Window {
-            set_title: Some("Mitt första Relm4-fönster"),
-            set_default_size: (300, 200),
+            set_title: Some("Mitt första Relm4-fönster"),   // Sätter fönstertiteln
+            set_default_size: (800, 600),                   // Bredd och höjd vid start
+            set_resizable: true,                           // Hindrar användaren från att ändra storlek
+            //set_maximized: true,                          // Startar fönstret i helskärm
+            //set_decorations: false,                       // Tar bort systemramen/kryssknappen (borderless)
+            set_opacity: 0.9,                               // Gör fönstret en aning transparent
+
+            // HUVUDBOX (Motsvarar en vertikal StackPanel)
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 12,          // Avstånd mellan elementen (i pixlar)
+                set_margin_all: 15,       // "Padding" runt hela innerinnehållet
+
+                // 1. En rubrik (Label)
+                gtk::Label {
+                    set_label: "Välkommen till Relm4",
+                    // Vi kan styla texten med inbyggda CSS-klasser i GTK
+                    add_css_class: "title-1",
+                },
+
+                // 2. Ett textinmatningsfält (Motsvarar TextBox)
+                gtk::Entry {
+                    set_placeholder_text: Some("Skriv något här..."),
+                    // Gör så att textfältet suger åt sig allt ledigt vertikalt utrymme
+                    set_vexpand: true,
+                    set_valign: gtk::Align::Center,
+                },
+
+                // 3. RAD MED KNAPPAR (Motsvarar en horisontell StackPanel längst ner)
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_spacing: 6,
+                    // Knuffar hela denna box till höger sida
+                    set_halign: gtk::Align::End,
+
+                    gtk::Button {
+                        set_label: "Avbryt",
+                    },
+
+                    gtk::Button {
+                        set_label: "OK",
+                        // Gör knappen blå/accentfärgad (GTK standard-styling)
+                        add_css_class: "suggested-action",
+                    },
+                }
+            }
+
+
+
+
         }
     }
 }
@@ -53,6 +101,6 @@ impl SimpleComponent for App {
 // 4. Starta applikationen i main-funktionen
 fn main() {
     let app = RelmApp::new("se.exempel.enkelt_fonster");
-    app.run::<App>(());
     let _ = dbusreceiver::main();
+    app.run::<App>(());
 }
